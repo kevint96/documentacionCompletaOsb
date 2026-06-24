1af2033f521303b1aac36b7f8386b20ac9fc2ae1
Kevin Torres <ktorres@soaint.com>
2026-06-24 08:07:38 -0500

    pruebas

--- proyecto_completo_osb.py
+++ proyecto_completo_osb.py
@@ -1239,33 +1239,33 @@ def generar_operaciones_expuestas_http(project_path,operacion_a_documentar):
     combined_services = {}
     found = False  # Variable para rastrear si se encuentra la operación
     
-   #print_with_line_number(f"operacion_a_documentar: {operacion_a_documentar}")
+    print_with_line_number(f"operacion_a_documentar: {operacion_a_documentar}")
     wsdl_operations_map = extraer_operaciones_expuestas_http(project_path,operacion_a_documentar)
     
-   #print_with_line_number(f"wsdl_operations_map: {wsdl_operations_map}")
+    print_with_line_number(f"wsdl_operations_map: {wsdl_operations_map}")
     # Recorriendo el diccionario
     for wsdl_path, data in wsdl_operations_map.items():
         # Desempaquetar la tupla
         operations, project_name, service_name, osb_file_path, pipeline_path, service_url, capa_proyecto = data
         
-       #print_with_line_number(f"wsdl_path: {wsdl_path}")
-       #print_with_line_number(f"operations: {operations}")
-       #print_with_line_number(f"project_name: {project_name}")
-       #print_with_line_number(f"service_name: {service_name}")
-       #print_with_line_number(f"osb_file_path: {osb_file_path}")
-       #print_with_line_number(f"pipeline_path: {pipeline_path}")
-       #print_with_line_number(f"service_url: {service_url}")
-       #print_with_line_number(f"capa_proyecto: {capa_proyecto}")
+        print_with_line_number(f"wsdl_path: {wsdl_path}")
+        print_with_line_number(f"operations: {operations}")
+        print_with_line_number(f"project_name: {project_name}")
+        print_with_line_number(f"service_name: {service_name}")
+        print_with_line_number(f"osb_file_path: {osb_file_path}")
+        print_with_line_number(f"pipeline_path: {pipeline_path}")
+        print_with_line_number(f"service_url: {service_url}")
+        print_with_line_number(f"capa_proyecto: {capa_proyecto}")
         if operacion_a_documentar:
             if operacion_a_documentar in operations:
 
                 services_for_operations_exp = extraer_operaciones_pipeline_exp(pipeline_path, operacion_a_documentar)
                             
-               #print_with_line_number(f"services_for_operations_exp: {services_for_operations_exp}")
+                print_with_line_number(f"services_for_operations_exp: {services_for_operations_exp}")
                 
                 services_for_operations_ebs = extraer_operaciones_pipeline_ebs(project_path,services_for_operations_exp)
                 
-               #print_with_line_number(f"services_for_operations_ebs: {services_for_operations_ebs}")
+                print_with_line_number(f"services_for_operations_ebs: {services_for_operations_ebs}")
 
                 for operation, proxy_list in services_for_operations_exp.items():
                     combined_services[operation] = {'Proxy': proxy_list, 'Referencia': []}
@@ -1281,11 +1281,11 @@ def generar_operaciones_expuestas_http(project_path,operacion_a_documentar):
 
                 services_for_operations_exp = extraer_operaciones_pipeline_exp(pipeline_path, operations)
                             
-               #print_with_line_number(f"services_for_operations_exp: {services_for_operations_exp}")
+                print_with_line_number(f"services_for_operations_exp: {services_for_operations_exp}")
                 
                 services_for_operations_ebs = extraer_operaciones_pipeline_ebs(project_path,services_for_operations_exp)
                 
-               #print_with_line_number(f"services_for_operations_ebs: {services_for_operations_ebs}")
+                print_with_line_number(f"services_for_operations_ebs: {services_for_operations_ebs}")
 
                 for operation, proxy_list in services_for_operations_exp.items():
                     combined_services[operation] = {'Proxy': proxy_list, 'Referencia': []}
@@ -1296,11 +1296,11 @@ def generar_operaciones_expuestas_http(project_path,operacion_a_documentar):
                     else:
                         combined_services[operation] = {'Proxy': [], 'Referencia': reference_list}
                 
-       #print_with_line_number(f"combined_services: {combined_services}")
+        print_with_line_number(f"combined_services: {combined_services}")
                 
         combined_services2 = separar_ebs_abc_business(project_path,combined_services)
                 
-       #print_with_line_number(f"combined_services2: {combined_services2}")
+        print_with_line_number(f"combined_services2: {combined_services2}")
                 
         #generar_diagramas_operaciones(project_name,combined_services2)
             
@@ -1473,13 +1473,13 @@ def get_namespace_prefixes(root):
 def extraer_operaciones_pipeline_exp(pipeline_path, operations):
     services_for_operations = defaultdict(set)
     
-   #print_with_line_number("***************************** INICIO EXTRACT SERVICE OPERATIONS*********************************************")
+    print_with_line_number("***************************** INICIO EXTRACT SERVICE OPERATIONS*********************************************")
 
     if not (pipeline_path.endswith('.Pipeline') and os.path.isfile(pipeline_path)):
        #print_with_line_number("Archivo no válido o no encontrado.")
         return services_for_operations
 
-   #print_with_line_number(f"pipeline_path: {pipeline_path}")
+    print_with_line_number(f"pipeline_path: {pipeline_path}")
 
     # Cargar el archivo XML
     with open(pipeline_path, 'r', encoding="utf-8") as f:
